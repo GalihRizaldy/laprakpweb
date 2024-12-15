7d770ad587ac0f0969b7f5a678beadfcb382a7da
Rizaldyyy <galihrizaldi47@gmail.com>
2024-12-16 01:51:25 +0700

    Add files via upload

---
+++ modul4&5/m3.js
@@ -0,0 +1,12 @@
+function pesan() {
+    alert("ini adalah latihan");
+}
+
+function consolee() {
+    console.log("its work");
+}
+
+function web() {
+    document.write("succes <br>")
+    document.write("<a href=m3.html>kembali</a>")
+}
